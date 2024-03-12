002d55c1ed3c4780ad222f1dbf3257b1ed016abf
Frederic MARTIN <fmartin.fr@gmail.com>
2024-03-12 17:28:36 +0100

    services/waiter:add lru cache into api v2

--- services/waiter/waiter/api/router_v2.py
+++ services/waiter/waiter/api/router_v2.py
@@ -1,6 +1,6 @@
 import requests
 import logging
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from http import HTTPStatus
 from waiter.config import COOK_API_BASE_URL
 from waiter.api.models import Menu
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 @lru_cache(maxsize=16)
-@api_router_v2.get("/restaurant/{restaurant_id}/menu", response_model=Menu)
-async def get_menu(restaurant_id: int):
+def get_menu_from_cook(restaurant_id: int):
+    logger.info(get_menu_from_cook.cache_info())
     url = COOK_API_BASE_URL + "/v1/restaurant/" + str(restaurant_id) + "/menu"
 
     logger.info(f"requesting {url}")
@@ -27,3 +27,8 @@ async def get_menu(restaurant_id: int):
         raise HTTPException(status_code=r.status_code)
 
     return r.json()
+
+
+@api_router_v2.get("/restaurant/{restaurant_id}/menu", response_model=Menu)
+async def get_menu(restaurant_id: int, menu=Depends(get_menu_from_cook)):
+    return menu
