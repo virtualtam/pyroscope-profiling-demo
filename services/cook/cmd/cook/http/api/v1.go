647b3ca6cea58d5b6158a6664849f925be8595db
VirtualTam <virtualtam@flibidi.net>
2024-03-12 15:39:26 +0100

    services/cook: add Pyroscope tags
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- services/cook/cmd/cook/http/api/v1.go
+++ services/cook/cmd/cook/http/api/v1.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"context"
 	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
+	"github.com/grafana/pyroscope-go"
 	"gorm.io/gorm"
 )
 
@@ -18,19 +20,21 @@ func (s *Server) registerV1API() {
 }
 
 func (s *Server) v1getRestaurantMenu(w http.ResponseWriter, r *http.Request) {
-	restaurantID := chi.URLParam(r, "restaurantID")
+	pyroscope.TagWrapper(context.Background(), pyroscope.Labels("api", "v1"), func(ctx context.Context) {
+		restaurantID := chi.URLParam(r, "restaurantID")
 
-	menu, err := s.restaurantService.Menu(restaurantID)
+		menu, err := s.restaurantService.Menu(restaurantID)
 
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
 
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 
-	render.JSON(w, r, menu)
+		render.JSON(w, r, menu)
+	})
 }
